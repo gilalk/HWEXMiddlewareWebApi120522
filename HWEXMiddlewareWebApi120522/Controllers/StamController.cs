dea7123690e2b32083440a9508cf74d95a2fac5d
Gil Alkobi <86377486+gilalk@users.noreply.github.com>
2022-05-20 05:20:43 +0300

    final commit

---
+++ HWEXMiddlewareWebApi120522/Controllers/StamController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HWEXMiddlewareWebApi120522.Controllers
+{
+    [ApiController]
+    [Route("test/[action]")]
+    public class StamController
+    {
+        public string Action1()
+        {
+            return "Action 1";
+        }
+
+        public string Action2()
+        {
+            return "Action 2";
+        }
+    }
+}
